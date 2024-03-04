37bfc165cc2ca859eef903a85c4fb6e7e1c5716b
app1 <saieesh.soma1919@gmail.com>
2024-03-04 19:42:04 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
 import React from 'react'
 
 export default function Fruits() {
-//    let fruits= [
-//     {
-//         name:"Apple",description: "This is dummy text",price:3,
-//         image: "https://5.imimg.com/data5/AK/RA/MY-68428614/apple.jpg",
-//     },
-//     {
-//         name:"Banana",description: "This is dummy text",price:2,
-//         image:"https://5.imimg.com/data5/ANDROID/Default/2022/2/UT/VN/SF/29630129/product-jpeg-250x250.jpg",
-//     },
-//     {
-//         name:"Orange",description: "This is dummy text",price:4,
-//         image:"https://5.imimg.com/data5/SELLER/Default/2022/9/OH/QA/NX/45117192/fresh-nagpur-orange-500x500.png"
-//     },
-//     {
-//         name:"Papaya",description: "This is dummy text",price:6,
-//         image:"https://5.imimg.com/data5/SELLER/Default/2021/3/QL/AL/BR/86539219/papaya-fruit-500x500.jpg"
-//     },
-//     {
-//         name:"Cherry",description: "This is dummy text",price:7,
-//         image:"https://5.imimg.com/data5/ANDROID/Default/2024/2/390090761/JF/XV/GV/85845923/product-jpeg-500x500.jpeg"
-//     },
-//     {
-//         name:"Strawberry",description: "This is dummy text",price:9,
-//         image:"https://5.imimg.com/data5/SELLER/Default/2023/12/373118026/XZ/HR/YH/28942657/fresh-organic-strawberries-500x500.jpg"
-//     }
-// ]
+   let fruits= [
+    {
+        name:"Apple",description: "This is dummy text",price:3,
+        image: "https://5.imimg.com/data5/AK/RA/MY-68428614/apple.jpg",
+    },
+    {
+        name:"Banana",description: "This is dummy text",price:2,
+        image:"https://5.imimg.com/data5/ANDROID/Default/2022/2/UT/VN/SF/29630129/product-jpeg-250x250.jpg",
+    },
+    {
+        name:"Orange",description: "This is dummy text",price:4,
+        image:"https://5.imimg.com/data5/SELLER/Default/2022/9/OH/QA/NX/45117192/fresh-nagpur-orange-500x500.png"
+    },
+    {
+        name:"Papaya",description: "This is dummy text",price:6,
+        image:"https://5.imimg.com/data5/SELLER/Default/2021/3/QL/AL/BR/86539219/papaya-fruit-500x500.jpg"
+    },
+    {
+        name:"Cherry",description: "This is dummy text",price:7,
+        image:"https://5.imimg.com/data5/ANDROID/Default/2024/2/390090761/JF/XV/GV/85845923/product-jpeg-500x500.jpeg"
+    },
+    {
+        name:"Strawberry",description: "This is dummy text",price:9,
+        image:"https://5.imimg.com/data5/SELLER/Default/2023/12/373118026/XZ/HR/YH/28942657/fresh-organic-strawberries-500x500.jpg"
+    }
+]
 
   return (
     <div>
-        {/* {fruits.map((value,index)=>(
+        {fruits.map((value,index)=>(
             <div class="container">
                 <div><img src={value.image} width={175} height={150} alt='fruitimg'></img></div>
                 <div id="fname">{value.name}</div>
@@ -40,8 +40,7 @@ export default function Fruits() {
             </div> 
 
             ))
-        } */
-        <h2>Hello World</h2>}
+        }
     </div>
   )
 }
