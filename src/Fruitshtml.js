ab76102f2e258f3794d6edd0698d34c9312ec0fe
app1 <saieesh.soma1919@gmail.com>
2024-03-04 18:00:22 +0530

    initiall commit

--- src/Fruitshtml.js
+++ src/Fruitshtml.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import "./App.css"
-import App from './App.js'
+import Fruits from './App.js'
 
 export default function Fruitshtml() {
   return (
    <> <div><h1 id='titlee'>Fruits</h1></div>
     <hr></hr>
-    <div id="products" class="main"><App /></div>
+    <div id="products" class="main"><Fruits /></div>
     </>
     )
 }
